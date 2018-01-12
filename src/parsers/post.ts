834d6e9f71e98d479503ac9b0f325f027c96056e
Dustin Schau <dustinschau@gmail.com>
2018-01-12 13:15:11 -0600

    feat: add an attempt to guess post categories

--- src/parsers/post.ts
+++ src/parsers/post.ts
@@ -1,5 +1,49 @@
 import { Author, Post } from '../interfaces';
 
+const CATEGORIES = {
+  java: 'Java',
+  javascript: 'JavaScript',
+  devops: 'Devops',
+  mobile: 'Mobile'
+}
+
+const CATEGORY_MAP = {
+  javascript: CATEGORIES.javascript,
+  react: CATEGORIES.javascript,
+  angular: CATEGORIES.javascript,
+  jquery: CATEGORIES.javascript,
+  node: CATEGORIES.javascript,
+  nodejs: CATEGORIES.javascript,
+  java: CATEGORIES.java,
+  mobile: CATEGORIES.mobile,
+  android: CATEGORIES.mobile,
+  ios: CATEGORIES.mobile,
+  xcode: CATEGORIES.mobile,
+  devops: CATEGORIES.devops,
+  gradle: CATEGORIES.java,
+  spring: CATEGORIES.java,
+  groovy: CATEGORIES.java,
+  'spring-boot': CATEGORIES.java,
+  kotlin: CATEGORIES.java,
+  tomcat: CATEGORIES.java,
+  docker: CATEGORIES.devops,
+  terraform: CATEGORIES.devops
+}
+
+function getCategory(tags: Set<string>) {
+  if (tags.size === 0) {
+    return 'Unknown';
+  }
+
+  const category = Object.keys(CATEGORY_MAP).find(category => tags.has(category));
+
+  if (category) {
+    return CATEGORY_MAP[category];
+  }
+  
+  return 'Unknown';
+}
+
 export function parsePosts(input: Object, authors: Author[], tagName = 'item'): Post[] {
   const posts = input[tagName];
   if (!posts) {
